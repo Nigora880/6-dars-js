05f16707863a0635d2a0a0e1f20ca1e5f743bf8b
Nigora880 <nx165880@gmail.com>
2025-05-04 15:47:17 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,84 @@
+// 1
+
+// let person = {
+//     name: "Asilbek",
+//     age: "20",
+//     job: "meneger",
+//     interest: "game",
+//     hobby: "sleep",
+//     number: "+998998640825",
+//     username: "M.A.I"
+// }
+// console.log(Object.entries(person));
+
+// 2
+
+// const person1 = Object.assign({name: "Asilbek"},{age: 20})
+// const person2 =Object.assign ({interest: "game"},{hobby: "sleep"})
+// console.log(person1);
+// console.log(person2);
+
+// 3
+
+// let numbers = [];
+// for (let i = 0; i < 10; i++) {
+//   numbers.push(Math.floor(Math.random() * 10));
+// }
+
+// console.log("1-array:", numbers);
+// // lastVALUE=lValue
+// let lValue = numbers[numbers.length - 1];
+// // newValue=nValue   
+// let nValue = lValue + 1;
+// numbers.push(nValue);
+
+// console.log("2-array:", numbers);
+
+// 4
+
+// let numbers = [];
+// for (let i = 0; i < 10; i++) {
+//   numbers.push(Math.floor(Math.random() * 10));
+// }
+
+// console.log("1-array:", numbers);
+
+// let sum = numbers.for((a, b) => a + b, 0);
+// console.log("yig'indi:", sum);
+
+// if (numbers[0] % 2 === 0) {
+//   numbers.unshift(1);
+
+//   console.log("juft");
+// } else {
+
+//     numbers.push(1);
+//   console.log("toq");
+// }
+
+// console.log("2-array:", numbers);
+
+// 5
+
+// let arr = [10, 20, 30, 40, 50];
+
+// console.log("1-array:", arr);
+
+// let firstElement = arr[0];
+// let lastElement = arr[arr.length - 1];
+
+// arr[0] = lastElement;
+// arr[arr.length - 1] = firstElement;
+
+// console.log("array:", arr);
+
+// 6
+let numbers = [];
+for (let i = 0; i < 10; i++) {
+  numbers.push(Math.floor(Math.random() * 10));
+}
+numbers.push for (let i = 0; i < 10; i++) {
+  numbers.push(Math.floor(Math.random() * 10));
+}
+
+console.log (numbers);
